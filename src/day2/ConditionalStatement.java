a2b94c334d97c360c2f215a50057ea5e6b764fcc
Amany Elhgadrawy <amanyelkhadrawy283@gmail.com>
2026-01-21 00:58:31 +0200

    ConditionalStatement

--- src/day2/ConditionalStatement.java
+++ src/day2/ConditionalStatement.java
@@ -1,4 +1,48 @@
 package day2;
 
+import java.util.Scanner;
+
 public class ConditionalStatement {
+    public static void main(String[] args) {
+       /*
+       //Example1
+        int age=18;
+        if(age>=18){
+            System.out.println("you are eligible");
+        }
+        else{
+            System.out.println("you are not eligible");
+        }
+
+        //Example2
+        Scanner Scanner=new Scanner(System.in);
+        int number=Scanner.nextInt();
+        if(number%2==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("ODD");
+        }
+        Scanner.close();
+
+
+        Scanner Scanner =new Scanner (System.in);
+        int number=Scanner.nextInt();
+        if(number>0){
+            System.out.println("Positive");
+
+        }
+        else if(number<0){
+            System.out.println("Negative");
+
+        }
+        else{
+            System.out.println("Zero");
+        }
+        Scanner.close();
+
+        */
+        //Example3
+
+    }
 }
