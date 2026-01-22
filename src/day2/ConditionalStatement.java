352f5930ca5ae8f86e764bb74ec96df1b853fef0
Amany Elhgadrawy <amanyelkhadrawy283@gmail.com>
2026-01-22 15:01:34 +0200

    Day2 classes

--- src/day2/ConditionalStatement.java
+++ src/day2/ConditionalStatement.java
@@ -71,7 +71,7 @@ public class ConditionalStatement {
             case 4:System.out.println("Wednesday");break;
             case 5:System.out.println("Thursday");break;
             case 6:System.out.println("Friday");break;
-            case 7:System.out.println("staurday");break;
+            case 7:System.out.println("saturday");break;
             default:System.out.println("invalid input");
         }
 
